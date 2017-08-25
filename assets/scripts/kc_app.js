d8112873bc2b956815b8bc0229f261ef1bc1c0f8
Jen <Jennifer.Hiar@allstate.com>
2017-08-25 09:49:12 -0500

    Socal media icons, form stuff, new phone images to match text, mobile phone image view, menu fix, key issue image carousel

---
+++ assets/scripts/kc_app.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function() {
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    autoplay: true,
+    arrows: false,
+    caption: true,
+  });
+
+});
